cec0e3f16055738694b626531d8d11909589e0e2
Aarush <aarush@Aarushs-MacBook-Pro.local>
2025-09-01 12:38:57 +1000

    Initial commit for Task 3.2 C# web app

---
+++ MyTask3CsharpApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyTask3CsharpApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
